93b42e2b586902ff393219ec2b388d66f1251094
Tejiri Ibihwiori <tejiri@Tejiris-MacBook-Pro.local>
2020-12-28 12:53:16 -0600

    store routes and controller looks good and tidy..we move

---
+++ controllers/store_controller.js
@@ -0,0 +1,133 @@
+//===========
+// dependencies
+//==================
+
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/store.js');
+
+
+
+
+//===================
+//Edit Route
+//===================
+
+router.get('/:id/edit', (req, res) => {
+  Product.findById(req.params.id, (error, foundProduct) => {
+    res.render(
+      'edit.ejs',
+      {
+        product: foundProduct
+      }
+    )
+  });
+});
+
+//===================
+//Update Route
+//===================
+
+router.put('/:id', (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedProduct) => {
+    res.redirect(`/home/${req.params.id}`);
+  });
+});
+
+
+//===================
+//Seed Route
+//===================
+
+router.get('/seed', (req, res) => {
+  Product.create(
+    [
+      {
+        name: 'westly',
+        description: 'shoe',
+        price: 234,
+        qty: 3,
+        img: 'https://wallpaperaccess.com/full/30100.jpg'
+      }
+    ],
+    (err, data) => {
+      res.redirect('/home')
+    }
+  )
+})
+
+
+//===================
+//New Route
+//===================
+
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+
+//===================
+//Show Route
+//===================
+
+
+router.get('/:id', (req, res) => {
+    Product.findById(req.params.id, (error, foundProduct) => {
+        res.render(
+            'show.ejs',
+            {
+                product:foundProduct
+            }
+        );
+    })
+});
+
+//===================
+//Buy Route
+//===================
+
+router.put('/:id/buy', (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, {$inc: {qty: -1} }, (error, boughtProduct) => {
+    res.redirect(`/home/${req.params.id}`)
+  })
+})
+
+//===================
+//Delete Route
+//===================
+
+router.delete('/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (error, deletedProduct) => {
+    res.redirect('/home')
+  })
+})
+
+
+//===================
+//Index Route
+//===================
+
+router.get('/', (req, res) => {
+  Product.find({}, (error, allProducts) => {
+    res.render(
+      'index.ejs',
+      {
+        products: allProducts
+      }
+    )
+  })
+
+})
+
+//===================
+//Create / Post Route
+//===================
+
+router.post('/', (req, res) => {
+    Product.create(req.body, (error, createdProduct) => {
+        res.redirect('/home')
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+
+//===============
+// dependencies
+//================
+
 const express = require('express');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
@@ -5,8 +10,7 @@ const app = express();
 const db = mongoose.connection
 require('dotenv').config();
 
-// requiring Routes
-const Product = require('./models/store.js');
+
 
 const PORT = process.env.PORT || 3000
 
@@ -20,130 +24,27 @@ mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('MONGODB_URI connected: 🧜🏿‍♀️🧜🏿‍♀️🧜🏿‍♀️🧜🏿‍♀️🧜🏿‍♀️🧜🏿‍♀️🧜🏿‍♀️'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-//___________________
+
+
+//===================
 //Middleware
-//___________________
+//===================
 
-//use public folder for static assets
 app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
-
-//___________________
-// Routes
-
-//___________________
-// edit Route
-//__________________
-
-app.get('/home/:id/edit', (req, res) => {
-  Product.findById(req.params.id, (error, foundProduct) => {
-    res.render(
-      'edit.ejs',
-      {
-        product: foundProduct
-      }
-    )
-  });
-});
-
-app.put('/home/:id', (req, res) => {
-  Product.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedProduct) => {
-    res.redirect(`/home/${req.params.id}`);
-  });
-});
-
-// app.put('/home/:id', (req, res) => {
-//   res.send(req.body)
-// });
-
-// seed route
-app.get('/home/seed', (req, res) => {
-  Product.create(
-    [
-      {
-        name: 'westly',
-        description: 'shoe',
-        price: 234,
-        qty: 3,
-        img: 'https://wallpaperaccess.com/full/30100.jpg'
-      }
-    ],
-    (err, data) => {
-      res.redirect('/home')
-    }
-  )
-})
-
-
-// new route
-app.get('/home/new', (req, res) => {
-    res.render('new.ejs');
-});
-
-
-// show route
-app.get('/home/:id', (req, res) => {
-    Product.findById(req.params.id, (error, foundProduct) => {
-        res.render(
-            'show.ejs',
-            {
-                product:foundProduct
-            }
-        );
-    })
-});
-
-// buy route
-//___________
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+const storeController = require('./controllers/store_controller.js');
+app.use('/home', storeController)
 
 
 
-app.put('/home/:id/buy', (req, res) => {
-  Product.findByIdAndUpdate(req.params.id, {$inc: {qty: -1} }, (error, boughtProduct) => {
-    res.redirect(`/home/${req.params.id}`)
-  })
-})
 
-// delete route
+//===================
+//listerner
+//===================
 
-app.delete('/home/:id', (req, res) => {
-  Product.findByIdAndRemove(req.params.id, (error, deletedProduct) => {
-    res.redirect('/home')
-  })
-})
-
-
-// index route
-app.get('/home', (req, res) => {
-  Product.find({}, (error, allProducts) => {
-    res.render(
-      'index.ejs',
-      {
-        products: allProducts
-      }
-    )
-  })
-
-})
-
-// post route
-app.post('/home', (req, res) => {
-    Product.create(req.body, (error, createdProduct) => {
-        res.redirect('/home')
-    })
-})
-
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Guidian Angel🧞‍♂️🧞‍♂️🧞‍♂️🧞‍♂️🧞‍🧞‍♂️🧞‍♂️🧞‍♂️🧞‍♂️🧞',PORT));
+app.listen(PORT, () => console.log( 'Rafael..🧞‍♂️🧞‍♂️🧞‍♂️🧞‍♂️🧞‍🧞‍♂️🧞‍♂️🧞‍♂️🧞‍♂️🧞', PORT));
